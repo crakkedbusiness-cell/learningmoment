d861e6944461f4e0458f9ea2ec2e23115a076f58
crakkedbusiness-cell <crakkedbusiness@gmail.com>
2025-08-30 13:35:19 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,80 +1,107 @@
 const express = require('express')
+var morgan = require('morgan')
 const app = express()
-const cors = require('cors')
-
-app.use(cors())
-
 app.use(express.json())
 
-let notes = [
-    {
-        id: "1",
-        content: "HTML is easy",
-        important: true
-      },
-      {
-        id: "2",
-        content: "Browser can execute only JavaScript",
-        important: false
-      },
-      {
-        id: "3",
-        content: "GET and POST are the most important methods of HTTP protocol",
-        important: true
-      }
+
+morgan.token('getBody', function getBody (req) {
+  return req.body.name
+})
+
+app.use(morgan(':getBody :method :url :response-time'))
+
+let persons = [
+  { 
+    "id": "1",
+    "name": "Arto Hellas", 
+    "number": "040-123456"
+  },
+  { 
+    "id": "2",
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
+  },
+  { 
+    "id": "3",
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": "4",
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+  }
 ]
 
-app.get('/api/notes/:id', (request, response) => {
-  const id = request.params.id
-  const note = notes.find(note => note.id === id)
-  response.json(note)
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
 
-  if (note) { //all JS objects are truthy, hence they evaluate to true 
-    response.json(note)
+app.get('/info', (request, response) => {
+  entries = persons.length
+  response.send(`Phonebook has info for ${entries} people `)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  const number = persons.find(num => num.id === id)
+
+  if (number) { //all JS objects are truthy, hence they evaluate to true 
+    response.json(number)
   } else {
     response.status(404).end()
-  }
+  }  
 })
 
-app.get('/api/notes', (request, response) => {
-  response.json(notes)
-})
 
-app.delete('/api/notes/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response) => {
   const id = request.params.id
-  notes = notes.filter(note => note.id !== id) //this is a good way to remove stuff that you want to remove using a filter
+  persons = persons.filter(persons => persons.id !== id) //this is a good way to remove stuff that you want to remove using a filter
 
   response.status(204).end()
 })
 
 const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => Number(n.id)))
+  const maxId = persons.length > 0
+    ? Math.max(...persons.map(p => Number(p.id)))
     : 0
   return String(maxId + 1)
 }
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/persons', (request, response) => {
   const body = request.body
 
-  if (!body.content) {
+  if (!body) {
     return response.status(400).json({ 
       error: 'content missing' 
     })
   }
 
-  const note = {
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number
+  }
+
+ /* const person = {
+    id: 
     content: body.content,
     important: body.important || false,
     id: generateId(),
-  }
+  }*/
 
-  notes = notes.concat(note)
+  persons = persons.concat(person)
 
-  response.json(note)
+  response.json(person)
 })
 
-const PORT = process.env.PORT || 3001
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })

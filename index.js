dbf8f621fc4dcfafcd414222238bbc14b808b30b
Taha <crakkedbusiness@gmail.com>
2025-09-01 13:32:25 +0100

    updating the repo and bringing it up to date before starting work

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express')
 var morgan = require('morgan')
 const app = express()
 app.use(express.json())
+const cors = require('cors')
+
+app.use(cors())
 
 
 morgan.token('getBody', function getBody (req) {
@@ -101,7 +104,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
